ea8d55946ceb30da550ed62eff6bfebed650554f
Nusmir <nusmir.smajlovic@hotmail.com>
2020-12-09 07:51:38 +0100

    Add project files.

---
+++ Inlämning/IMemento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inlämning
+{
+   public  interface IMemento
+    {
+
+        string GetName();
+
+        string GetState();
+
+        DateTime GetDate();
+    }
+}
---
+++ Inlämning/Program.cs
@@ -0,0 +1,32 @@
+﻿using Inlämning;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Client code.
+        Originator originator = new Originator(Console.ReadLine());
+        Caretaker caretaker = new Caretaker(originator);
+
+        caretaker.Backup();
+        originator.DoSomething();
+
+        caretaker.Backup();
+        originator.DoSomething();
+
+        caretaker.Backup();
+        originator.DoSomething();
+
+        Console.WriteLine();
+        caretaker.ShowHistory();
+
+        Console.WriteLine("\nClient: Now, let's rollback!\n");
+        caretaker.Undo();
+
+        Console.WriteLine("\n\nClient: Once more!\n");
+        caretaker.Undo();
+
+        Console.WriteLine();
+    }
+}
